b8a275bd89303f965c668c58fa8937ddb8fc730d
jsyczhanghao <jsyczhanghao@sina.com>
2014-12-02 13:57:44 +0800

    rename

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = function(content, file){
         }
 
         //feather中require的模块 要么是以/开头 要么是可map读取，
-        //这边兼容下fis对require路径转换给PAFFE带来的bug
+        //这边兼容下fis对require路径转换给feather带来的bug
         content = content.replace(REQUIRE_REG, function(_0, _1, _2){
             var file = feather.file.wrap(_2);
             return "require('" + (file.exists() ? file.subpath : _2) + "')";
